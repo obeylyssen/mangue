0c30ca3df846f79ea4274181e316c131b7359951
obeylyssen <obeylyssen@gmail.com>
2015-04-22 11:14:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ var map = AmCharts.makeChart("chartdiv", {
             latitude: 48.132689,
             longitude: 11.568615,
             label:"Opti",
+            labelShiftX:2,
             
         },  {
             zoomLevel: 5,
