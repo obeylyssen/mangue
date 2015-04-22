b3c65dd7d3ac885c6bc7bbcc4731858982d22ffb
obeylyssen <obeylyssen@gmail.com>
2015-04-22 13:45:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var map = AmCharts.makeChart("chartdiv", {
             labelColor:"#fff601", 
             labelFontSize:20,
             balloonText: "[[description]]",
-            description:"<h1>Je suis un lezard</h1>"
+            description:"<style>h1{color:red;};</style><h1>Je suis un lezard</h1>"
             
         },  {
             zoomLevel: 5,
