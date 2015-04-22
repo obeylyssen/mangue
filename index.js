15679c6d0d11d8be4710a76079270604ca3e097f
obeylyssen <obeylyssen@gmail.com>
2015-04-22 13:17:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ var map = AmCharts.makeChart("chartdiv", {
             latitude: 48.8567,
             longitude: 2.3510,
             label:"SILMO",
+            labelShiftX:9,
            labelPosition:"left",
             labelColor:"#fff601",
             labelFontSize:20
